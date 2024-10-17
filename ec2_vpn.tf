de0da33c8e1a249263eb8c4dde3298d45906582b
Pavlo Shevtsov <paulshevtsov@gmail.com>
2024-10-17 18:13:02 +0300

    Add VPN instance

--- ec2_vpn.tf
+++ ec2_vpn.tf
@@ -1,18 +1,20 @@
+/*
 #Server One
-data "template_file" "init_data" {
+data "template_file" "init_data_vpn" {
   template = file("user_data/server_one.sh")
 }
+*/
 
-resource "aws_instance" "server_one" {
+resource "aws_instance" "server_vpn" {
   ami                         = var.ami_ubuntu_22_base
-  instance_type               = var.inst_type_server
+  instance_type               = "t3a.small"
   user_data                   = data.template_file.init_data.rendered
   security_groups             = [aws_security_group.data_server.name]
   key_name                    = "operator"
   source_dest_check           = false
   associate_public_ip_address = true
   root_block_device {
-    volume_size           = 60
+    volume_size           = 20
     delete_on_termination = true
     volume_type           = "gp3"
     throughput            = 125
@@ -25,31 +27,11 @@ resource "aws_instance" "server_one" {
 */
 
   tags = {
-    Name = "keb-server-one"
+    Name = "keb-server-vpn"
   }
 
   volume_tags = {
-    Name = "keb-server-one"
+    Name = "keb-server-vpn"
   }
 
 }
-
-#EIP for SERVER_ONE
-resource "aws_eip" "server_one" {
-  domain = "vpc"
-
-  lifecycle {
-    prevent_destroy = false
-  }
-
-  tags = {
-    Name     = "eip-server-one"
-  }
-}
-
-resource "aws_eip_association" "server_one" {
-  instance_id   = aws_instance.server_one.id
-  allocation_id = aws_eip.server_one.id
-}
-
-
--- sg.tf
+++ sg.tf
@@ -1,11 +1,51 @@
 resource "aws_security_group" "data_server" {
   name_prefix = "data_server-sg"
 
+  tags = {
+    "Name" = "data-server"
+  }
+
+#SSH
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+#HTTP (For test)
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+#Node_exporter
+  ingress {
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["172.31.0.0/16"]
+  }
+
+#Portainer
+  ingress {
+    from_port   = 9010
+    to_port     = 9010
+    protocol    = "tcp"
+    cidr_blocks = ["213.109.238.102/32",
+                   "46.175.248.249/32"]
+  }
+
+#Postman
+  ingress {
+    from_port   = 6901
+    to_port     = 6901
+    protocol    = "tcp"
+    cidr_blocks = ["213.109.238.102/32",
+                   "46.175.248.249/32",
+                   "213.81.189.100/32"]
   }
 
   egress {
@@ -19,11 +59,41 @@ resource "aws_security_group" "data_server" {
 resource "aws_security_group" "monitor" {
   name_prefix = "monitor-sg"
 
+  tags = {
+    "Name" = "monitor"
+  }
+
+#SSH
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+#Node_exporter
+  ingress {
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["172.31.0.0/16"]
+  }
+
+#Prometheus
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["213.109.238.102/32",
+                   "172.31.0.0/16"]
+  }
+
+#Grafana
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["213.109.238.102/32"]
   }
 
   egress {
@@ -34,3 +104,17 @@ resource "aws_security_group" "monitor" {
   }
 }
 
+resource "aws_security_group" "vpn" {
+  name_prefix = "vpn-sg"
+
+  tags = {
+    "Name" = "vpc"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
